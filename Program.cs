60df1fb7321d20164fe9f1aa443e8800954e12eb
KaraevaLinura <linura.karaeva.87@mail.ru>
2022-08-25 21:36:45 +0600

    дз5

--- Program.cs
+++ Program.cs
@@ -1,1 +1,46 @@
-﻿
+﻿/*internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Clear(); // интересная фишка!!! очистка консоли!
+        Console.WriteLine($"Задача 34. Количество чётных чисел в массиве:\n");*/
+
+        int[] numbers = new int[10];
+ 
+        void FillArray(int[] array, int min, int max)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = new Random().Next(min, max);
+            }
+        }
+ 
+        void WriteArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+            Console.WriteLine();
+        }
+ 
+        int QuantityPositive(int[] array)
+        {
+            int quantity = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] % 2 == 0)
+                {
+                    quantity++;
+                }
+            }
+            return quantity;
+        }
+ 
+        FillArray(numbers, 100, 1000);
+        WriteArray(numbers);
+        Console.WriteLine();
+ 
+        int quantity = QuantityPositive(numbers);
+        Console.WriteLine($"Количество чётных чисел в массиве: {quantity}");
+    
